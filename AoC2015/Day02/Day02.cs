dc6118ddf0151c7eabe511ae369072593e855256
Jake Turner <jake@unity3d.com>
2020-02-21 06:16:18 +0000

    Start of Day02

--- AoC2015/Day02/Day02.cs
+++ AoC2015/Day02/Day02.cs
@@ -3,62 +3,18 @@ using System.IO;
 
 /*
 
---- Day 2: 1202 Program Alarm ---
+--- Day 2: I Was Told There Would Be No Math ---
 
-On the way to your gravity assist around the Moon, your ship computer beeps angrily about a "1202 program alarm". On the radio, an Elf is already explaining how to handle the situation: "Don't worry, that's perfectly norma--" The ship computer bursts into flames.
+The elves are running low on wrapping paper, and so they need to submit an order for more. They have a list of the dimensions (length l, width w, and height h) of each present, and only want to order exactly as much as they need.
 
-You notify the Elves that the computer's magic smoke seems to have escaped. "That computer ran Intcode programs like the gravity assist program it was working on; surely there are enough spare parts up there to build a new Intcode computer!"
+Fortunately, every present is a box (a perfect right rectangular prism), which makes calculating the required wrapping paper for each gift a little easier: find the surface area of the box, which is 2*l*w + 2*w*h + 2*h*l. The elves also need a little extra paper for each present: the area of the smallest side.
 
-An Intcode program is a list of integers separated by commas (like 1,0,0,3,99). To run one, start by looking at the first integer (called position 0). Here, you will find an opcode - either 1, 2, or 99. The opcode indicates what to do; for example, 99 means that the program is finished and should immediately halt. Encountering an unknown opcode means something went wrong.
+For example:
 
-Opcode 1 adds together numbers read from two positions and stores the result in a third position. The three integers immediately after the opcode tell you these three positions - the first two indicate the positions from which you should read the input values, and the third indicates the position at which the output should be stored.
+A present with dimensions 2x3x4 requires 2*6 + 2*12 + 2*8 = 52 square feet of wrapping paper plus 6 square feet of slack, for a total of 58 square feet.
+A present with dimensions 1x1x10 requires 2*1 + 2*10 + 2*10 = 42 square feet of wrapping paper plus 1 square foot of slack, for a total of 43 square feet.
+All numbers in the elves' list are in feet. How many total square feet of wrapping paper should they order?
 
-For example, if your Intcode computer encounters 1,10,20,30, it should read the values at positions 10 and 20, add those values, and then overwrite the value at position 30 with their sum.
-
-Opcode 2 works exactly like opcode 1, except it multiplies the two inputs instead of adding them. Again, the three integers after the opcode indicate where the inputs and outputs are, not their values.
-
-Once you're done processing an opcode, move to the next one by stepping forward 4 positions.
-
-For example, suppose you have the following program:
-
-1,9,10,3,2,3,11,0,99,30,40,50
-For the purposes of illustration, here is the same program split into multiple lines:
-
-1,9,10,3,
-2,3,11,0,
-99,
-30,40,50
-The first four integers, 1,9,10,3, are at positions 0, 1, 2, and 3. Together, they represent the first opcode (1, addition), the positions of the two inputs (9 and 10), and the position of the output (3). To handle this opcode, you first need to get the values at the input positions: position 9 contains 30, and position 10 contains 40. Add these numbers together to get 70. Then, store this value at the output position; here, the output position (3) is at position 3, so it overwrites itself. Afterward, the program looks like this:
-
-1,9,10,70,
-2,3,11,0,
-99,
-30,40,50
-Step forward 4 positions to reach the next opcode, 2. This opcode works just like the previous, but it multiplies instead of adding. The inputs are at positions 3 and 11; these positions contain 70 and 50 respectively. Multiplying these produces 3500; this is stored at position 0:
-
-3500,9,10,70,
-2,3,11,0,
-99,
-30,40,50
-Stepping forward 4 more positions arrives at opcode 99, halting the program.
-
-Here are the initial and final states of a few more small programs:
-
-1,0,0,0,99 becomes 2,0,0,0,99 (1 + 1 = 2).
-2,3,0,3,99 becomes 2,3,0,6,99 (3 * 2 = 6).
-2,4,4,5,99,0 becomes 2,4,4,5,99,9801 (99 * 99 = 9801).
-1,1,1,4,99,5,6,0,99 becomes 30,1,1,4,2,5,6,0,99.
-Once you have a working computer, the first step is to restore the gravity assist program (your puzzle input) to the "1202 program alarm" state it had just before the last computer caught fire. To do this, before running the program, replace position 1 with the value 12 and replace position 2 with the value 2. What value is left at position 0 after the program halts?
-
-*/
-
-/* 
-
-Part2 : take the same input and find inputs for address 1 & 2 that result in a program result of 19690720
-
-inputs range 0-99
-
-Result is : 100 * value1 + value2
 */
 
 namespace Day02
@@ -67,36 +23,23 @@ namespace Day02
     {
         private Program(string inputFile, bool part1)
         {
-            var source = ReadProgram(inputFile);
+            var inputs = ReadInput(inputFile);
             if (part1)
             {
-                var result = RunProgram(source, 12, 2);
-                // What value is left at position 0 after the program halts?
-                Console.WriteLine($"Day02 : Result1 {result.Split(',')[0]}");
+                var result1 = -666;
+                Console.WriteLine($"Day02 : Result1 {result1}");
             }
             else
             {
-                for (int noun = 0; noun < 99; noun++)
-                {
-                    for (int verb = 0; verb < 99; verb++)
-                    {
-                        //Console.WriteLine($"noun:{noun} verb:{verb}");
-                        var result = RunProgram(source, noun, verb);
-                        var string0 = result.Split(',')[0];
-                        var value0 = Int32.Parse(string0);
-                        if (value0 == 19690720)
-                        {
-                            Console.WriteLine($"Day02 : Result2 {noun * 100 + verb}");
-                            return;
-                        }
-                    }
-                }
+                var result2 = -666;
+                Console.WriteLine($"Day02 : Result2 {result2}");
+                return;
             }
         }
 
-        private string ReadProgram(string inputFile)
+        private string[] ReadInput(string inputFile)
         {
-            var source = File.ReadAllText(inputFile);
+            var source = File.ReadAllLines(inputFile);
             return source;
         }
 
