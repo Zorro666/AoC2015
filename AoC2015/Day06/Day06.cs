108a96584638d532de9ce8194b853d29d19024fa
Jake Turner <jake@unity3d.com>
2020-02-23 09:10:18 +0000

    Day06 done

--- AoC2015/Day06/Day06.cs
+++ AoC2015/Day06/Day06.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
 
 /*
 
@@ -21,19 +19,39 @@ toggle 0,0 through 999,0 would toggle the first line of 1000 lights, turning off
 turn off 499,499 through 500,500 would turn off (or leave off) the middle four lights.
 After following the instructions, how many lights are lit?
 
+--- Part Two ---
+
+You just finish implementing your winning light pattern when you realize you mistranslated Santa's message from Ancient Nordic Elvish.
+
+The light grid you bought actually has individual brightness controls; each light can have a brightness of zero or more. The lights all start at zero.
+
+The phrase turn on actually means that you should increase the brightness of those lights by 1.
+
+The phrase turn off actually means that you should decrease the brightness of those lights by 1, to a minimum of zero.
+
+The phrase toggle actually means that you should increase the brightness of those lights by 2.
+
+What is the total brightness of all lights combined after following Santa's instructions?
+
+For example:
+
+turn on 0,0 through 0,0 would increase the total brightness by 1.
+toggle 0,0 through 999,999 would increase the total brightness by 2000000.
+
 */
 
 namespace Day06
 {
     class Program
     {
+        static readonly int GRID_SIZE = 1000;
         private Program(string inputFile, bool part1)
         {
             var instructions = AoC2015.Program.ReadLines(inputFile);
             if (part1)
             {
-                var result1 = HowManyLights(instructions);
-                long expected = 236;
+                var result1 = HowManyLights(instructions, false);
+                long expected = 543903;
                 Console.WriteLine($"Day06 : Result1 {result1}");
                 if (result1 != expected)
                 {
@@ -42,8 +60,8 @@ namespace Day06
             }
             else
             {
-                var result2 = -123;
-                long expected = 51;
+                var result2 = HowManyLights(instructions, true);
+                long expected = 14687245;
                 Console.WriteLine($"Day06 : Result2 {result2}");
                 if (result2 != expected)
                 {
@@ -52,9 +70,126 @@ namespace Day06
             }
         }
 
-        public static long HowManyLights(string[] instructions)
+        public static long HowManyLights(string[] instructions, bool useBrightness)
         {
-            return -2323;
+            var lights = new int[GRID_SIZE, GRID_SIZE];
+            for (var y = 0; y < GRID_SIZE; ++y)
+            {
+                for (var x = 0; x < GRID_SIZE; ++x)
+                {
+                    lights[x, y] = 0;
+                }
+            }
+
+            foreach (var line in instructions)
+            {
+                // turn <on>/<off> <x1>,<y1> through <x2,y2>
+                // toggle <x1>,<y1> through <x2,y2>
+                var tokens = line.Split(' ');
+                var rangeStart = -1;
+                var command = tokens[0];
+                int op = -2;
+                if (command == "turn")
+                {
+                    rangeStart = 2;
+                    var operation = tokens[1];
+                    if (operation == "on")
+                    {
+                        op = 1;
+                    }
+                    else if (operation == "off")
+                    {
+                        op = 0;
+                    }
+                    else
+                    {
+                        throw new InvalidOperationException($"Unknown operation {operation}");
+                    }
+                }
+                else if (command == "toggle")
+                {
+                    rangeStart = 1;
+                    op = 2;
+                }
+                else
+                {
+                    throw new InvalidOperationException($"Unknown command {command}");
+
+                }
+                var xy1Tokens = tokens[rangeStart].Split(',');
+                var x1 = int.Parse(xy1Tokens[0]);
+                var y1 = int.Parse(xy1Tokens[1]);
+                if (tokens[rangeStart + 1] != "through")
+                {
+                    throw new InvalidOperationException($"Failed xy range parsign {line} expected 'through' got {tokens[rangeStart + 1]}");
+                }
+                var xy2Tokens = tokens[rangeStart + 2].Split(',');
+                var x2 = int.Parse(xy2Tokens[0]);
+                var y2 = int.Parse(xy2Tokens[1]);
+                for (var y = y1; y <= y2; ++y)
+                {
+                    for (var x = x1; x <= x2; ++x)
+                    {
+                        if (op == 0)
+                        {
+                            if (useBrightness)
+                            {
+                                lights[x, y] = lights[x, y] - 1;
+                                if (lights[x, y] < 0)
+                                {
+                                    lights[x, y] = 0;
+                                }
+                            }
+                            else
+                            {
+                                lights[x, y] = 0;
+                            }
+                        }
+                        else if (op == 1)
+                        {
+                            if (useBrightness)
+                            {
+                                lights[x, y] = lights[x, y] + 1;
+                            }
+                            else
+                            {
+                                lights[x, y] = 1;
+                            }
+                        }
+                        else if (op == 2)
+                        {
+                            if (useBrightness)
+                            {
+                                lights[x, y] = lights[x, y] + 2;
+                            }
+                            else
+                            {
+                                if (lights[x, y] == 0)
+                                {
+                                    lights[x, y] = 1;
+                                }
+                                else
+                                {
+                                    lights[x, y] = 0;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            throw new InvalidOperationException($"Invalid op {op}");
+                        }
+                    }
+                }
+            }
+            var brightness = 0;
+            for (var y = 0; y < GRID_SIZE; ++y)
+            {
+                for (var x = 0; x < GRID_SIZE; ++x)
+                {
+                    brightness += lights[x, y];
+                }
+            }
+            return brightness;
         }
 
         public static void Run()
--- AoC2015/Day06/Day06Tests.cs
+++ AoC2015/Day06/Day06Tests.cs
@@ -8,12 +8,23 @@ namespace Day06
         [Test]
         [TestCase("turn on 0,0 through 999,999", 1000 * 1000)]
         [TestCase("toggle 0,0 through 999,0", 1000)]
-        [TestCase("toggle off 499,499 through 500,500", 0)]
-        [TestCase("toggle on 499,499 through 500,500", 4)]
+        [TestCase("turn off 499,499 through 500,500", 0)]
+        [TestCase("turn on 499,499 through 500,500", 4)]
         public void HowManyLights(string line, long expected)
         {
             var instructions = new string[] { line };
-            Assert.That(Program.HowManyLights(instructions), Is.EqualTo(expected));
+            Assert.That(Program.HowManyLights(instructions, false), Is.EqualTo(expected));
+        }
+
+        [Test]
+        [TestCase("turn on 0,0 through 0,0", 1)]
+        [TestCase("toggle 0,0 through 999,999", 2 * 1000 * 1000)]
+        [TestCase("turn off 499,499 through 500,500", 0)]
+        [TestCase("turn on 499,499 through 500,500", 4)]
+        public void GetBrightness(string line, long expected)
+        {
+            var instructions = new string[] { line };
+            Assert.That(Program.HowManyLights(instructions, true), Is.EqualTo(expected));
         }
     }
 }
