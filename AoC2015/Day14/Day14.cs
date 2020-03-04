5baf2ce9e0091a085ac0657a5105828331bb11f6
Jake Turner <jake@unity3d.com>
2020-03-04 05:13:59 +0000

    Day14 part1 done - start of part2

--- AoC2015/Day14/Day14.cs
+++ AoC2015/Day14/Day14.cs
@@ -13,11 +13,27 @@ For example, suppose you have the following Reindeer:
 Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.
 Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds.
 
-After one second, Comet has gone 14 km, while Dancer has gone 16 km. After ten seconds, Comet has gone 140 km, while Dancer has gone 160 km. On the eleventh second, Comet begins resting (staying at 140 km), and Dancer continues on for a total distance of 176 km. On the 12th second, both reindeer are resting. They continue to rest until the 138th second, when Comet flies for another ten seconds. On the 174th second, Dancer flies for another 11 seconds.
+After one second, Comet has gone 14 km, while Dancer has gone 16 km. After ten seconds, Comet has gone 140 km, while Dancer has gone 160 km. On the eleventh second, Comet begins resting (staying at 140 km), and Dancer continues on for a total distance of 176 km. On the 12th second, both reindeer are resting. 
+They continue to rest until the 138th second, when Comet flies for another ten seconds. 
+On the 174th second, Dancer flies for another 11 seconds.
 
 In this example, after the 1000th second, both reindeer are resting, and Comet is in the lead at 1120 km (poor Dancer has only gotten 1056 km by that point). So, in this situation, Comet would win (if the race ended at 1000 seconds).
 
 Given the descriptions of each reindeer (in your puzzle input), after exactly 2503 seconds, what distance has the winning reindeer traveled?
+
+Your puzzle answer was 2640.
+
+--- Part Two ---
+
+Seeing how reindeer move in bursts, Santa decides he's not pleased with the old scoring system.
+
+Instead, at the end of each second, he awards one point to the reindeer currently in the lead. (If there are multiple reindeer tied for the lead, they each get one point.) He keeps the traditional 2503 second time limit, of course, as doing otherwise would be entirely ridiculous.
+
+Given the example reindeer from above, after the first second, Dancer is in the lead and gets one point. He stays in the lead until several seconds into Comet's second burst: after the 140th second, Comet pulls into the lead and gets his first point. Of course, since Dancer had been in the lead for the 139 seconds before that, he has accumulated 139 points by the 140th second.
+
+After the 1000th second, Dancer has accumulated 689 points, while poor Comet, our old champion, only has 312. So, with the new scoring system, Dancer would win (if the race ended at 1000 seconds).
+
+Again given the descriptions of each reindeer (in your puzzle input), after exactly 2503 seconds, how many points does the winning reindeer have?
 
 */
 
@@ -25,13 +41,25 @@ namespace Day14
 {
     class Program
     {
+        struct Rule
+        {
+            public string name;
+            public int speed;
+            public int speedTime;
+            public int restTime;
+        };
+
+        static Rule[] sRules;
+
         private Program(string inputFile, bool part1)
         {
+            var lines = AoC2015.Program.ReadLines(inputFile);
+            ParseInput(lines);
             if (part1)
             {
-                var result1 = -666;
+                var result1 = WinningDistance(2503);
                 Console.WriteLine($"Day14 Result1:{result1}");
-                var expected = -123;
+                var expected = 2640;
                 if (result1 != expected)
                 {
                     throw new InvalidProgramException($"Part1 is broken {result1} != {expected}");
@@ -47,6 +75,62 @@ namespace Day14
                     throw new InvalidProgramException($"Part2 is broken {result2} != {expected}");
                 }
             }
+        }
+
+        public static void ParseInput(string[] lines)
+        {
+            sRules = new Rule[lines.Length];
+            int i = 0;
+            foreach (var line in lines)
+            {
+                var tokens = line.Split(' ');
+                if (tokens.Length != 15)
+                {
+                    throw new InvalidProgramException($"Invalid line '{line}' expected 15 tokens {tokens.Length}");
+                }
+                ref Rule rule = ref sRules[i];
+                rule.name = tokens[0];
+                rule.speed = int.Parse(tokens[3]);
+                rule.speedTime = int.Parse(tokens[6]);
+                rule.restTime = int.Parse(tokens[13]);
+                ++i;
+            }
+        }
+
+        public static int Distance(string reindeer, int time)
+        {
+            foreach (var rule in sRules)
+            {
+                if (rule.name == reindeer)
+                {
+                    return Distance(rule.speed, rule.speedTime, rule.restTime, time);
+                }
+            }
+            return -1;
+        }
+
+        static int Distance(int speed, int speedTime, int restTime, int time)
+        {
+            var distance = 0;
+            var cycleTime = speedTime + restTime;
+            var fullCycles = time / cycleTime;
+            distance += fullCycles * speedTime * speed;
+            var remainderTime = time - fullCycles * cycleTime;
+            remainderTime = Math.Min(remainderTime, speedTime);
+            distance += remainderTime * speed;
+
+            return distance;
+        }
+
+        public static int WinningDistance(int time)
+        {
+            var maxDistance = int.MinValue;
+            foreach (var rule in sRules)
+            {
+                var distance = Distance(rule.speed, rule.speedTime, rule.restTime, time);
+                maxDistance = Math.Max(maxDistance, distance);
+            }
+            return maxDistance;
         }
 
         public static void Run()
--- AoC2015/Day14/Day14Tests.cs
+++ AoC2015/Day14/Day14Tests.cs
@@ -7,26 +7,44 @@ namespace Day14
     [TestFixture]
     public class Tests
     {
-        static readonly string[] inputA = {
-"10 ORE => 10 A",
-"1 ORE => 1 B",
-"7 A, 1 B => 1 C",
-"7 A, 1 C => 1 D",
-"7 A, 1 D => 1 E",
-"7 A, 1 E => 1 FUEL"
+        static readonly string[] reindeerRaceTest = {
+"Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.",
+"Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds."
         };
-        public static IEnumerable ParseInputCases => new[]
+
+        public static IEnumerable DistanceTests => new[]
         {
-            new TestCaseData(inputA, "A", "ORE").SetName("ParseInput.A A = 1 ORE").Returns(1.0),
+            new TestCaseData(reindeerRaceTest, "Comet", 1).SetName("Comet 1 = 14").Returns(14),
+            new TestCaseData(reindeerRaceTest, "Dancer", 1).SetName("Dancer 1 = 16").Returns(16),
+            new TestCaseData(reindeerRaceTest, "Comet", 10).SetName("Comet 10 = 140").Returns(140),
+            new TestCaseData(reindeerRaceTest, "Dancer", 10).SetName("Dancer 10 = 160").Returns(160),
+            new TestCaseData(reindeerRaceTest, "Comet", 11).SetName("Comet 11 = 140").Returns(140),
+            new TestCaseData(reindeerRaceTest, "Dancer", 11).SetName("Dancer 11 = 176").Returns(176),
+            new TestCaseData(reindeerRaceTest, "Comet", 12).SetName("Comet 12 = 140").Returns(140),
+            new TestCaseData(reindeerRaceTest, "Dancer", 12).SetName("Dancer 12 = 176").Returns(176),
+            new TestCaseData(reindeerRaceTest, "Comet", 1000).SetName("Comet 1000 = 1120").Returns(1120),
+            new TestCaseData(reindeerRaceTest, "Dancer", 1000).SetName("Dancer 1000 = 1056").Returns(1056)
         };
 
         [Test]
-        [TestCaseSource("ParseInputCases")]
-        public double ParseInput(string[] lines, string compoundOutput, string compoundInput)
+        [TestCaseSource("DistanceTests")]
+        public int ReindeerDistance(string[] lines, string reindeer, int time)
         {
-            //Program.ParseInput(lines);
-            //return Program.CompoundCost(compoundOutput, compoundInput);
-            return -666.0;
+            Program.ParseInput(lines);
+            return Program.Distance(reindeer, time);
+        }
+
+        public static IEnumerable WinningDistanceTests => new[]
+        {
+            new TestCaseData(reindeerRaceTest, 1000).SetName("WinningDistance 1000 = 1120").Returns(1120)
+        };
+
+        [Test]
+        [TestCaseSource("WinningDistanceTests")]
+        public int WinningDistance(string[] lines, int time)
+        {
+            Program.ParseInput(lines);
+            return Program.WinningDistance(time);
         }
     }
 }
