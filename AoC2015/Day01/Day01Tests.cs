b27164e5321b61414adb8886fc1d7b4a578d3e78
Jake Turner <jake@evansturner.co.uk>
2021-02-06 20:03:03 +0000

    Updates to latest .Net Core version

--- AoC2015/Day01/Day01Tests.cs
+++ AoC2015/Day01/Day01Tests.cs
@@ -6,6 +6,7 @@ namespace Day01
     public class Tests
     {
         [Test]
+        [TestCase("()", 0)]
         [TestCase("(())", 0)]
         [TestCase("()()", 0)]
         [TestCase("(((", 3)]
@@ -15,6 +16,9 @@ namespace Day01
         [TestCase("))(", -1)]
         [TestCase(")))", -3)]
         [TestCase(")())())", -3)]
+        [TestCase("", 0)]
+        [TestCase("()(()))))(((())))()", -3)]
+        [TestCase("Jake", 0)]
         public void WhichFloor(string directions, int expected)
         {
             Assert.That(Program.WhichFloor(directions), Is.EqualTo(expected));
@@ -22,6 +26,8 @@ namespace Day01
 
         [TestCase(")", -1, 1)]
         [TestCase("()())", -1, 5)]
+        [TestCase("()())", +1, 1)]
+        [TestCase("()())", 0, 2)]
         public void WhichIndex(string directions, int targetFloor, int expected)
         {
             Assert.That(Program.WhichIndex(directions, targetFloor), Is.EqualTo(expected));
